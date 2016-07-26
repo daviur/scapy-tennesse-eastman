12985d1185f794f0735dac26ece0bb3d972ddae5
daviur <github@davidurbina.net>
2016-07-26 02:55:35 -0400

    Formatted

--- te/te.py
+++ te/te.py
@@ -19,7 +19,6 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-
 """Scapy Dissector for CIP messages of the Field Communication Network of the
 Tennessee Eastman Testbed at the National Institute of Standards and Technology"""
 

ca9011b8921214d34c42e3ec2a701a19993c028b
daviur <github@davidurbina.net>
2016-07-26 02:56:57 -0400

    Removed .gitconfig

--- te/te.py
+++ te/te.py
@@ -19,6 +19,7 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+
 """Scapy Dissector for CIP messages of the Field Communication Network of the
 Tennessee Eastman Testbed at the National Institute of Standards and Technology"""
 
